f8f9b292fcbb961688c5a83e28f90701788a48db
Raven <y93209@gmail.com>
2019-12-18 08:22:52 +0800

    layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).scroll(function () {
+  if ($(window).width() > 0) {
+    if ($(this).scrollTop() > 10) {
+      $('header').addClass('boxshow');
+      $('.scroll_show').fadeIn(500);
+    } else {
+      $('header').removeClass('boxshow');
+      $('.scroll_show').fadeOut(100);
+    }
+  }
+});
+$('.drawer').drawer();
